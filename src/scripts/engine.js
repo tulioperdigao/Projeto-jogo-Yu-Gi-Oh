62baccf6bbbd5d1254e356900cef0a2502a0cc05
tulioperdigao <116309232+tulioperdigao@users.noreply.github.com>
2024-10-16 14:55:32 -0300

    criando enumeração das cartas

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -19,6 +19,34 @@ const state = {
     }
 };
 
+const pathImages = "src/assets/icons/"
+const cardData = [
+    {
+        id: 0,
+        name: "Blue Eyes White Dragon",
+        type: "Paper",
+        img: `${pathImages}dragon.png`,
+        WinOf: [1],
+        LoseOf: [2],
+    },
+    {
+        id: 1,
+        name: "Dark Magician",
+        type: "Rock",
+        img: `${pathImages}magician.png`,
+        WinOf: [2],
+        LoseOf: [0],
+    },
+    {
+        id: 2,
+        name: "Exodia",
+        type: "Scissors",
+        img: `${pathImages}exodia.png`,
+        WinOf: [0],
+        LoseOf: [1],
+    },
+]
+
 function init() {}
 
 init();
