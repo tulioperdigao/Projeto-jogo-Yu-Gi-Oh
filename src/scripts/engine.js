3bf28aba89a88cc70390520df44640f74af79de1
tulioperdigao <116309232+tulioperdigao@users.noreply.github.com>
2024-10-16 15:42:44 -0300

    assinatura das funções principais

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -19,7 +19,13 @@ const state = {
     }
 };
 
-const pathImages = "src/assets/icons/"
+const playerSides = {
+    player1: "player-field-card",
+    computer: "computer-field-card"
+};
+
+const pathImages = "src/assets/icons/";
+
 const cardData = [
     {
         id: 0,
@@ -45,8 +51,20 @@ const cardData = [
         WinOf: [0],
         LoseOf: [1],
     },
-]
+];
 
-function init() {}
+async function drawCards(cardNumbers, fieldSide) {
+    for (let i = 0; i < cardNumbers; i++) {
+        const randomIdCard = await getRandomCardId();
+        const cardImage = await createCardImage(randomIdCard, fieldSide);
+
+        document.getElementById(fieldSide).appendChild(cardImage);
+    }
+};
+
+function init() {
+    drawCards(5, playerSides.player1);
+    drawCards(5, playerSides.computer);
+};
 
 init();
